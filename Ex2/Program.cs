b342887c7b41c59698e14a89f3eca793c54b42d0
Nessa <la-nessa@mail.ru>
2023-01-10 22:52:29 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Задайте значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int x, int y)
+{
+    if (x == y)
+    {
+        return y;
+    }
+    if (x > y)
+    {
+        return y + SumNumbers(x, y + 1);
+    }
+    else
+    {
+        return y + SumNumbers(x, y - 1);
+    }
+}
+
+Console.Write("Сумма натуральных элементов в промежутке от M до N равна: ");
+Console.WriteLine(SumNumbers(N, M));
+
