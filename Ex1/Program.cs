5b05e31f544621fa4b5f61cf7d47872139f27027
Nessa <la-nessa@mail.ru>
2023-01-10 21:48:13 +0300

    Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задайте значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int GetNumbers(int x)
+{
+    Console.WriteLine(x);
+    if (x == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return x - GetNumbers(x-1);
+
+    }
+}
+
+GetNumbers(N);
