4b6e0f9260a6df1bb62d47762978222eb117f49f
Nessa <la-nessa@mail.ru>
2023-01-11 23:50:08 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задайте значение неотрицательного числа m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение неотрицательного числа n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int x, int y)
+{
+    if (x == 0)
+    {
+        return y + 1;
+    }
+    else
+    {
+        if (y == 0)
+        {
+            return Akkerman(x-1, 1);
+        }
+        else
+        {
+            return Akkerman(x -1, Akkerman(x, y - 1));
+        }
+    }
+}
+
+Console.Write("Результат вычисления функции Аккермана равен: ");
+Console.WriteLine(Akkerman(m, n));
